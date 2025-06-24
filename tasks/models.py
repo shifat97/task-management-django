263a954222b038e25cbfbb2f40e229e4cdf9fcc2
shifat97 <shifatbinreza2@gmail.com>
2025-06-24 18:10:43 +0600

    add module 05 contents

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, default=1)
+    assigned_to = models.ManyToManyField(Employee)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class TaskDetail(models.Model):
+    HIGH = 'H'
+    LOW = 'L'
+    MEDIUM = 'M'
+
+    PRIORITY_OPTIONS = (
+        (HIGH, 'High'),
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium')
+    )
+
+    task = models.OneToOneField(Task, on_delete=models.CASCADE)
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
