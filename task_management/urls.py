4e8cf88e75c4d246dc458902052b51b63a986f5b
shifat97 <shifatbinreza2@gmail.com>
2025-07-10 21:34:28 +0600

    add: loging and logout url added

--- task_management/urls.py
+++ task_management/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from users.views import home
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from core.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('', home),
     path('tasks/', include("tasks.urls")),
     path('users/', include("users.urls"))
 ] + debug_toolbar_urls()
--- users/views.py
+++ users/views.py
@@ -1,14 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from users.forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 # abc@123#$d
-
-def home(request):
-    return HttpResponse("<h1>Home Page</h1>")
 
 def sign_up(request):
     if request.method == 'GET':
@@ -28,3 +26,21 @@ def sign_up(request):
             #     print("Passwords are not same")
 
     return render(request, "register.html", {"form": form})
+
+def sign_in(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'login.html')
+
+def sign_out(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('sign_in')
